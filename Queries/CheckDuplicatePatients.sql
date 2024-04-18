a06deb5dcafe669b6d6c6000fbca8b524395dc25
cbrianbet <cbrianbet@outlook.com>
2024-04-18 19:25:19 +0300

    updated patient dups query

--- Queries/CheckDuplicatePatients.sql
+++ Queries/CheckDuplicatePatients.sql
@@ -1,11 +1,11 @@
-SELECT COUNT(*)
-                    FROM
-                    (
-                      SELECT patientPK
-                      FROM [DWAPICentral].[dbo].[PatientExtract]
-                      GROUP BY patientPK
-                      HAVING COUNT(*) > 1
-                    ) AS dup
-                    JOIN [DWAPICentral].[dbo].[PatientExtract] AS pe
-                    ON dup.patientPK = pe.patientPK
-                    WHERE pe.sitecode = :mfl_code;
+SELECT
+    COUNT(*)
+FROM
+(
+  SELECT [PatientCccNumber] PatientID
+  FROM [DWAPICentral].[dbo].[PatientExtract]
+INNER JOIN [DWAPICentral].[dbo].[Facility] F ON [FacilityId]  = F.Id  AND F.Voided=0
+WHERE Code = :mfl_code
+  GROUP BY PatientCccNumber
+  HAVING COUNT(*) > 1
+) AS dup
